e76340553b96d7c0e14cec944cc300ad67f5de77
Manthee1 <dinja.martin@gmail.com>
2024-09-26 15:49:08 +0200

    Fixed user migration column names and missing column

--- backend/database/migrations/1726666333399_create_users_table.ts
+++ backend/database/migrations/1726666333399_create_users_table.ts
@@ -17,10 +17,11 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id') // Primary Key
 
-      table.string('fistname').notNullable()
-      table.string('lastname').notNullable()
+      table.string('first_name').notNullable()
+      table.string('last_name').notNullable()
       table.string('username').notNullable().unique()
       table.string('email').notNullable().unique()
+      table.string('password').notNullable()
 
       table.timestamp('created_at')
       table.timestamp('updated_at')
