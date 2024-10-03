e888f0caf25bcef3e57e82413e0922c27df504bb
MartinH2k3 <martin.horsky.2003@gmail.com>
2024-10-03 20:16:34 +0200

    changed column name in table

--- backend/database/migrations/1727257020278_create_channels_table.ts
+++ backend/database/migrations/1727257020278_create_channels_table.ts
@@ -7,7 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
 
-      table.string('channel_name').notNullable()
+      table.string('name').notNullable()
       table.boolean('isPrivate').notNullable().defaultTo(false)
       table.text('description').nullable()
 
