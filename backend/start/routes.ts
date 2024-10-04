f551bf345687ad6bb11dbc1ea597eb1d23a9f345
MartinH2k3 <martin.horsky.2003@gmail.com>
2024-10-04 16:50:28 +0200

    fixed bug where user wasnt loaded into session

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -8,13 +8,20 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
+const ChannelsController = () => import('#controllers/channels_controller')
 const AuthController = () => import('#controllers/auth_controller')
 
 router.get('/auth', async ({ auth, response }) => {
   const authenticated = await auth.check()
   return response.status(authenticated ? 200 : 401).send({ authenticated: authenticated })
 })
-router.post('/register', [AuthController, 'register'])
-router.post('/login', [AuthController, 'login'])
+router
+  .group(() => {
+    router.post('/register', [AuthController, 'register'])
+    router.post('/login', [AuthController, 'login'])
+  })
+  .use(middleware.auth())
 router.post('/logout', [AuthController, 'logout'])
 router.get('/authWS', [AuthController, 'authWS'])
+router.post('/c/join', [ChannelsController, 'join'])
