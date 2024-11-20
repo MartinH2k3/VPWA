5bf13f6374e1f97b4f6bf49aaed3575401fc679e
Manthee1 <dinja.martin@gmail.com>
2024-11-20 14:20:17 +0100

    Added push notification handler

---
+++ frontend/src/notifications.ts
@@ -0,0 +1,64 @@
+import { AppVisibility, Notify } from 'quasar';
+
+
+Notification.requestPermission().then((result) => {
+  if (result === 'granted') {
+    console.log('Notification permission granted');
+  }
+});
+
+
+interface NotifyOptions {
+  type?: 'positive' | 'negative' | 'warning' | 'info' | 'ongoing',
+  timeout?: number,
+  action?: Function;
+  color?: string,
+  textColor?: string,
+  icon?: string,
+  detail?: string,
+}
+
+
+const defaultOptions: NotifyOptions = {
+  type: 'info',
+  timeout: 5000,
+  action: () => { },
+  color: 'primary',
+  textColor: 'white',
+  icon: 'favicon.ico',
+  detail: '',
+};
+
+
+export function notify(message: string, options?: NotifyOptions) {
+  options = { ...defaultOptions, ...options };
+
+  if (!(AppVisibility.appVisible)) {
+    const notification = new Notification(message, {
+      body: options.detail,
+      icon: options.icon,
+    });
+
+    notification.onclick = () => {
+      if (options.action) options.action();
+    };
+  }
+
+
+  Notify.create({
+    message: message,
+    type: options.type,
+    timeout: options.timeout,
+    // actions: options.actions,
+    color: options.color,
+    textColor: options.textColor,
+    icon: options.icon,
+  });
+
+}
+
+
+
+
+
+
