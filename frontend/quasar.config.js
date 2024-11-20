bfeb9b3cccddea005f1d4e166096b33b4474ce09
Manthee1 <dinja.martin@gmail.com>
2024-11-20 14:28:41 +0100

    Added AppVisibility plugin

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -46,7 +46,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
       target: {
-        browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
+        browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node20'
       },
 
@@ -103,7 +103,8 @@ module.exports = configure(function (/* ctx */) {
 
       // Quasar plugins
       plugins: [
-        'Notify'
+        'Notify',
+        'AppVisibility',
       ]
     },
 
@@ -120,7 +121,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli-vite/developing-ssr/configuring-ssr
     ssr: {
       // ssrPwaHtmlFilename: 'offline.html', // do NOT use index.html as name!
-                                          // will mess up SSR
+      // will mess up SSR
 
       // extendSSRWebserverConf (esbuildConf) {},
       // extendPackageJson (json) {},
@@ -131,7 +132,7 @@ module.exports = configure(function (/* ctx */) {
       // manualPostHydrationTrigger: true,
 
       prodPort: 3000, // The default port that the production server should use
-                      // (gets superseded if process.env.PORT is specified at runtime)
+      // (gets superseded if process.env.PORT is specified at runtime)
 
       middlewares: [
         'render' // keep this as last one
@@ -146,12 +147,12 @@ module.exports = configure(function (/* ctx */) {
       manifestFilename: 'manifest.json',
       useCredentialsForManifestTag: false,
       // useFilenameHashes: true,
-      extendGenerateSWOptions (cfg) {
+      extendGenerateSWOptions(cfg) {
         cfg.skipWaiting = false
         cfg.clientsClaim = false
         cfg.runtimeCaching = [
           {
-            urlPattern:  new RegExp('/login'),
+            urlPattern: new RegExp('/login'),
             handler: 'NetworkFirst',
           },
         ]
